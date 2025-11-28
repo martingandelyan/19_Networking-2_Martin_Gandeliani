48883271d6533f0b55c65702ea6ea8f81ea666da
Martin Gandeliani <martingandelyan@gmail.com>
2025-11-28 17:59:04 +0400

    Initial Commit

---
+++ 19_Networking-2_Martin_Gandeliani/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  19_Networking-2_Martin_Gandeliani
+//
+//  Created by Martin on 28.11.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
